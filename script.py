acdde6a8a8c97d3fe4349795e699ea4f9509c7a0
g-zeng <guo@Guos-MacBook-Pro.local>
2020-04-07 21:22:58 -0700

    python script

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-import os 
+import os
+import shutil
 
 
 print('Hello World!')
-os.rename('/tmp/file.txt', 'dest.txt')
+# os.rename('/tmp/file.txt', 'dest.txt')
+shutil.move('/tmp/file.txt', 'dest.txt')
